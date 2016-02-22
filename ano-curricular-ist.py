f7af467b230ad80fe198327e6305b5c6633bc52b
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2016-02-22 18:59:00 +0000

    refactoring

--- ano-curricular-ist.py
+++ ano-curricular-ist.py
@@ -1,20 +1,40 @@
-#AC = min( int( ECTS + 24)/60+1; numero de anos do curso)
+################################################################################
+#Formula de Calculo -> AC = min( int( ECTS + 24)/60+1; numero de anos do curso)#
+################################################################################
+
+### Functions ###
+
+def calculaCreditosNecessariosParaProximoAno(creditosFeitos, nroAnosCurso):
+	creditosExtra = 0.5
+	concluido = False
+	proximoAnoCurricular = calculaAnoCurricularCorrente(creditosFeitos, nroAnosCurso) + 1
+	if proximoAnoCurricular > nroAnosCurso:
+		print "Frequentas o ultimo ano curricular do curso."
+		return 0
+	while concluido == False:
+		tentativa = calculaAnoCurricularCorrente(creditosFeitos + creditosExtra, nroAnosCurso)
+		if tentativa >= proximoAnoCurricular:
+			concluido = True
+		else:
+			creditosExtra = creditosExtra + 0.5
+	return creditosExtra
+
+def calculaAnoCurricularCorrente(creditosFeitos, nroAnosCurso):
+	anoCurricular = min((creditosFeitos + 24) // 60 + 1, nroAnosCurso)
+	return anoCurricular
+
+
+### Interface ###
 
 anosCurso = input('Quantos anos tem o curso? ')
-creditosInput = input('Quantos creditos tens aprovados? ')
+if anosCurso > 10:
+	raise ValueError("O curso nao pode ter mais de 10 anos.")
+creditos = input('Quantos creditos tens aprovados? ')
+print ('O teu ano curricular -> ' + str(calculaAnoCurricularCorrente(creditos, anosCurso)))
+print ('Faltam-te ' + str(calculaCreditosNecessariosParaProximoAno(creditos, anosCurso)) + ' creditos para passar de ano.')
 
-anoCurricular = min((creditosInput + 24)//60+1, anosCurso)
 
-print ('O teu ano curricular -> ' + str(anoCurricular))
 
-creditosExtra=0.5
-concluded = False
-proxAnoCurricular = anoCurricular + 1
-while not concluded:
-	tentativa = min((creditosInput + creditosExtra + 24)//60+1, anosCurso)
-	if tentativa >= proxAnoCurricular:
-		concluded = True
-	else:
-		creditosExtra = creditosExtra + 0.5
 
-print ('Faltam-te ' + str(creditosExtra) + ' para passar de ano.')
+
+
