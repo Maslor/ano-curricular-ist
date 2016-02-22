bfca12b0d5ff21c8db3a99f581b05fde11812ab9
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2016-02-22 19:22:28 +0000

    added unitTests

--- ano-curricular-ist.py
+++ ano-curricular-ist.py
@@ -2,6 +2,8 @@
 #Formula de Calculo -> AC = min( int( ECTS + 24)/60+1; numero de anos do curso)#
 ################################################################################
 
+import unittest
+
 ### Functions ###
 
 def calculaCreditosNecessariosParaProximoAno(creditosFeitos, nroAnosCurso):
@@ -23,7 +25,6 @@ def calculaAnoCurricularCorrente(creditosFeitos, nroAnosCurso):
 	anoCurricular = min((creditosFeitos + 24) // 60 + 1, nroAnosCurso)
 	return anoCurricular
 
-
 ### Interface ###
 
 anosCurso = input('Quantos anos tem o curso? ')
@@ -32,6 +33,19 @@ if anosCurso > 10:
 creditos = input('Quantos creditos tens aprovados? ')
 print ('O teu ano curricular -> ' + str(calculaAnoCurricularCorrente(creditos, anosCurso)))
 print ('Faltam-te ' + str(calculaCreditosNecessariosParaProximoAno(creditos, anosCurso)) + ' creditos para passar de ano.')
+
+### Tests ###
+
+class TesteCalculoAnoCurricular(unittest.TestCase):
+	def test1Ano(self):
+		self.assertEqual(calculaAnoCurricularCorrente(10,3), 1)
+	def test2Ano(self):
+		self.assertEqual(calculaAnoCurricularCorrente(36,3), 2)
+	def test3Ano(self):
+		self.assertEqual(calculaAnoCurricularCorrente(96,3), 3)
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
